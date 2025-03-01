44d50925130c84bfa9ba643348e7a74a5926766c
root <root@rpc-aio-2.openstack.local>
2025-03-01 13:43:08 +0000

    fix the element placement

--- elements/rackspace/environment.d/01-defaults.bash
+++ elements/rackspace/environment.d/01-defaults.bash
@@ -1,3 +1,4 @@
 export DIB_DEBUG_TRACE=1
 export DIB_MODPROBE_BLACKLIST='usb-storage cramfs freevxfs jffs2 hfs hfsplus squashfs udf bluetooth'
 export DIB_CLOUD_INIT_DATASOURCES="Ec2, ConfigDrive, OpenStack"
+#export DIB_BOOTLOADER_DEFAULT_CMDLINE="modprobe.blacklist=bfa,lpfc nomodeset console=tty0 console=ttyS1,115200n8 audit=1 audit_backlog_limit=8192 net.ifnames=0 biosdevname=0"
